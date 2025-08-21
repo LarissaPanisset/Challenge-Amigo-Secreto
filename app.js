851d13d5f01451ae9caa1a1f75d6dd507aea1420
Larissa Panisset <panisset.larissa@gmail.com>
2025-08-21 13:37:15 -0300

    atualiza código

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+
+let nome = document.querySelector('input').value.trim();
+
+if (nome === "") {
+
+alert("Por favor, digite um nome!");
+
+} else { amigos.push(nome);
+
+exibirAmigos();
+
+} limparCampo();
+
+}
+
+function limparCampo() {
+
+let nomeInput = document.querySelector('input');
+
+nomeInput.value = '';
+
+}
+
+function exibirAmigos() {
+
+let listaAmigos = document.getElementById("listaAmigos");
+
+listaAmigos.innerHTML = "";
+
+for (let i = 0; i < amigos.length; i++) {
+
+listaAmigos.innerHTML += "<li>" + amigos[i] + "</li>"; }
+
+}
+
+function sortearAmigo() {
+
+if (amigos.length < 3) {
+
+alert("Adicione pelo menos 3 amigos para sortear!");
+
+return;
+
+}
+
+let indiceSorteado = Math.floor(Math.random() * amigos.length);
+
+let amigoSorteado = amigos[indiceSorteado];
+
+document.getElementById('resultado').innerHTML =
+
+"<p>O amigo sorteado é: <strong>" + amigoSorteado + "</strong></p>";
+}
